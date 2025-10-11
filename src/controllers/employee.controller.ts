e377df98868ad849c2c0dd455a5afeb948706acb
John-JuniorFofie <johnfofie31@gmail.com>
2025-10-11 20:58:12 +0000

    update employee controller to use bcrypt and enhance response structure

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import Employee from "../Models/employee.model.ts";
+import bcrypt from "bcrypt";
 
 // Create a new employee
 // export const createEmployee = async (req: Request, res: Response) => {
@@ -26,12 +27,12 @@ export const createEmployee = async (req: Request, res: Response) => {
 
     
     res.status(201).json({
-      message:"Employee created successfully",
+     success:true, message:"Employee created successfully",
       data: newUser,
     });
 
   } catch (error) {
-    res.status(400).json({ message: "Failed to create employee", error });
+    res.status(400).json({success:false, message: "Failed to create employee", error });
     return;
   }
 };
@@ -41,13 +42,13 @@ export const logInEmployee = async (req: Request, res:Response)=>{
     const {email, password}= req.body;
     if(!email|| !password){
 
-      return res.status(500).json({message:"please provide email and password"});
+      return res.status(500).json({success:false, message:"please provide email and password"});
     }
-    res. status(200).json({message: "login successful"});
+    res. status(200).json({success:true, message: "login successful"});
   
     }
     catch(error){
-    res.status(400).json({message:"failed to logIn", error})
+    res.status(400).json({success:false, message:"failed to logIn", error})
 };
   };
 
@@ -59,7 +60,7 @@ export const getAllEmployees = async (req: Request, res: Response) => {
     const employees = await Employee.find();
     res.status(200).json(employees);
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch employees", error });
+    res.status(500).json({success:false, message: "Failed to fetch employees", error });
   }
 };
 
@@ -68,11 +69,11 @@ export const getEmployeeById = async (req: Request, res: Response) => {
   try {
     const employee = await Employee.findById(req.params.id);
     if (!employee) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({success:false, message: "Employee not found" });
     }
     res.status(200).json(employee);
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving employee", error });
+    res.status(500).json({success:false, message: "Error retrieving employee", error });
   }
 };
 
@@ -85,11 +86,11 @@ export const updateEmployee = async (req: Request, res: Response) => {
       { new: true }
     );
     if (!updatedEmployee) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({success:false, message: "Employee not found" });
     }
     res.status(200).json(updatedEmployee);
   } catch (error) {
-    res.status(400).json({ message: "Failed to update employee", error });
+    res.status(400).json({success:false, message: "Failed to update employee", error });
   }
 };
 
@@ -98,11 +99,11 @@ export const deleteEmployee = async (req: Request, res: Response) => {
   try {
     const deleted = await Employee.findByIdAndDelete(req.params.id);
     if (!deleted) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({ success:false,message: "Employee not found" });
     }
-    res.status(200).json({ message: "Employee deleted successfully" });
+    res.status(200).json({success:true, message: "Employee deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Failed to delete employee", error });
+    res.status(500).json({ success:false, message: "Failed to delete employee", error });
   }
 };
 
